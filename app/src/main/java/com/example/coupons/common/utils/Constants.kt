6c8fac543b2170d12675e458197ce5a34bb97f1f
pedro.molina <pedro.molina@petroprix.com>
2023-01-24 15:23:38 +0100

    terminado

--- app/src/main/java/com/example/coupons/common/utils/Constants.kt
+++ app/src/main/java/com/example/coupons/common/utils/Constants.kt
@@ -1,4 +1,7 @@
 package com.example.coupons.common.utils
 
 object Constants {
+    const val ERROR_EXIST = "Error 01"
+    const val ERROR_LENGTH = "Error 02"
+    const val ERROR_UNKNOWN = "Unknown"
 }
