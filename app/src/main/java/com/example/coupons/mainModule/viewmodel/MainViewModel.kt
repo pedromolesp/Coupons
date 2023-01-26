e2ef5398f9b6484f3df14888c3a00a8762f1705f
pedro.molina <pedro.molina@petroprix.com>
2023-01-26 16:20:04 +0100

    terminado

--- app/src/main/java/com/example/coupons/mainModule/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/example/coupons/mainModule/viewmodel/MainViewModel.kt
@@ -1,4 +1,41 @@
 package com.example.coupons.mainModule.viewmodel
 
-class MainViewModel {
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.coupons.R
+import com.example.coupons.common.entity.CouponEntity
+import com.example.coupons.common.utils.getMessageErrorByCode
+import com.example.coupons.mainModule.model.MainRepository
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.coroutines.launch
+
+class MainViewModel : ViewModel() {
+    private val repository = MainRepository()
+
+    private val result = MutableLiveData<CouponEntity>()
+    fun getResult() = result
+
+    private val snackMsg = MutableLiveData<Int>()
+    fun getSnackbarMsg() = snackMsg
+
+    fun consultCouponByCode(code: String) {
+        viewModelScope.launch {
+            result.value = repository.consultCouponByCode(code)
+        }
+    }
+
+    fun saveCoupon(couponEntity: CouponEntity) {
+        viewModelScope.launch {
+
+            try {
+                repository.saveCoupon(couponEntity)
+                consultCouponByCode(couponEntity.code)
+                snackMsg.value = R.string.main_save_success
+            } catch (e: Exception) {
+
+                snackMsg.value = getMessageErrorByCode(e.message)
+            }
+        }
+    }
 }
