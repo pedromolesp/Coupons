2990f0b5ba4449c477489d1869440867ec75441d
pedro.molina <pedro.molina@petroprix.com>
2023-01-26 15:39:49 +0100

    terminado

--- app/src/main/java/com/example/coupons/mainModule/model/MainRepository.kt
+++ app/src/main/java/com/example/coupons/mainModule/model/MainRepository.kt
@@ -1,4 +1,19 @@
 package com.example.coupons.mainModule.model
 
+import com.example.coupons.common.entity.CouponEntity
+import com.example.coupons.common.utils.Constants
+import com.example.coupons.common.utils.validateText
+
 class MainRepository {
+
+    private val roomDatabase = RoomDatabase()
+
+    suspend fun consultCouponByCode(code: String) = roomDatabase.consultCouponByCode(code)
+    suspend fun saveCoupon(couponEntity: CouponEntity) {
+        if (validateText(couponEntity.code)) {
+            roomDatabase.saveCoupon(couponEntity)
+        } else {
+            throw Exception(Constants.ERROR_LENGTH)
+        }
+    }
 }
