5c54143f6ea4fb0073d56704c8a010c1a329569a
Pedro Molina <pedro.molina@petroprix.com>
2023-02-03 13:56:54 +0100

    test

--- app/src/test/java/com/example/coupons/common/utils/CouponUtilsKtTest.kt
+++ app/src/test/java/com/example/coupons/common/utils/CouponUtilsKtTest.kt
@@ -1,11 +1,18 @@
 package com.example.coupons.common.utils
 
+import com.cursosandroidant.coupons.common.utils.validateTextCode
 import org.junit.Assert.*
 import org.junit.Test
 
 class CouponUtilsKtTest {
     @Test
     fun validateTextCodeSuccessTest() {
-        assertTrue(false)
+        val code = "WELCOME"
+        assertTrue(validateTextCode(code))
+    }
+    @Test
+    fun validateTextCodeEmptyFailTest() {
+        val code = ""
+        assertFalse(validateTextCode(code))
     }
 }
