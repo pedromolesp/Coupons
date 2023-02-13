047bcc6d927172132c8366b7a86f08929fc85d71
pedro.molina <pedro.molina@petroprix.com>
2023-02-13 08:55:25 +0100

    Test

--- app/src/androidTest/java/com/example/coupons/MainActivityCreateTest.kt
+++ app/src/androidTest/java/com/example/coupons/MainActivityCreateTest.kt
@@ -1,11 +1,18 @@
 package com.example.coupons
 
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
 import com.cursosandroidant.coupons.mainModule.view.MainActivity
 import org.junit.Rule
 import org.junit.runner.RunWith
+import com.cursosandroidant.coupons.R
+import org.junit.Test
+
 
 @LargeTest
 @RunWith(AndroidJUnit4::class)
@@ -13,4 +20,9 @@ class MainActivityCreateTest {
     @get:Rule
     val activityRule = ActivityScenarioRule(MainActivity::class.java)
 
+    @Test
+    fun createCouponTest(){
+        val etCoupon = onView(withId(R.id.etCoupon))
+        etCoupon.check(matches(withText("")))
+    }
 }
